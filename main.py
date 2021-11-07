0fdfdb78e227c6600a95a3c3938b3cac49a2e26b
Librist-pijo <71023896+Librist-pijo@users.noreply.github.com>
2021-11-07 13:25:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Zad a.
+def fn_names(*names):
+    for name in names:
+        print(name)
+
+
+fn_names('Ala', 'Tomek', 'Rafał', 'Piotrek', 'Monika')
+
+
+# Zad b.
+# i
+def for_multiply(*nmbr):
+    for number in nmbr:
+        number * 2
+    return print(nmbr)
+
+
+for_multiply(5, 6, 7, 2, 3)
+# ii
