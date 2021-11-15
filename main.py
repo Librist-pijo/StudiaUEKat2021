6c6a05f4827e4803855fcb95d2631d2c5fe1475c
Librist-pijo <71023896+Librist-pijo@users.noreply.github.com>
2021-11-15 20:17:33 +0100

    upload lab_2

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 # Zad a.
-def fn_names(*names):
+def fn_names(names):
     for name in names:
         print(name)
 
 
-fn_names('Ala', 'Tomek', 'Rafał', 'Piotrek', 'Monika')
+names = ['Ala', 'Tomek', 'Rafał', 'Piotr', 'Monika']
+fn_names(names)
 
 
 # Zad b.
-# i
-def for_multiply(*nmbr):
-    for number in nmbr:
-        number * 2
-    return print(nmbr)
+# i.
+def for_multiply(numbers):
+    for i in range(len(numbers)):
+        numbers[i] = numbers[i] * 2
+    return print(numbers)
 
 
-for_multiply(5, 6, 7, 2, 3)
-# ii
+numbers = [1, 2, 3, 4, 5]
+for_multiply(numbers)
+
+
+# ii.
+def multiply(numbers):
+    numbers = [number * 2 for number in numbers]
+    return print(numbers)
+
+
+numbers = [1, 2, 3, 4, 5]
+for_multiply(numbers)
+
+
+# zad c.
+numbers = list(range(11))
+for number in numbers:
+    if number % 2 == 0 and number != 0:
+        print(number)
+
+"""
+Zapisałem sobie jako alternatywa, lista składowana która od razu ma liczby parzyste
+numbers = [number for number in range(11) if number % 2 == 0 and number != 0]
+print(numbers)
+"""
+
+# zad d.
+numbers = list(range(11))
+print(numbers[::2])
