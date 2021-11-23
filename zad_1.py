ad1cc87b7c078d5f50f23ea97bae29d396fea7e7
Librist-pijo <71023896+Librist-pijo@users.noreply.github.com>
2021-11-23 18:26:13 +0100

    Add files via upload

---
+++ zad_1.py
@@ -0,0 +1,22 @@
+#zad 1
+
+class Student:
+    def __init__(self, name: str, marks: int):
+        self.name = name
+        self.marks = marks
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def is_passed(self):
+        if self.marks > 50:
+            return True
+        else:
+            return False
+
+
+obj1 = Student(name='Ktoś', marks=0)
+obj1.marks = 51
+print(obj1.is_passed())
+obj1.marks = 49
+print(obj1.is_passed())
---
+++ zad_3.py
@@ -0,0 +1,56 @@
+import decimal
+
+
+class Property:
+    def __init__(self, area: str, rooms: int, price: decimal, address: str):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = address
+
+    def __str__(self):
+        return f"""
+               Property:
+                   area: {self.area}
+                   rooms: {self.rooms}
+                   price: {self.price}
+                   address: {self.address}
+               """
+
+
+class House(Property):
+    def __init__(self, plot: int, area: str, rooms: int, price: decimal, address: str):
+        super().__init__(area, rooms, price, address)
+        self.plot = plot
+
+    def __str__(self):
+        return f"""
+               House(Property):
+                   area: {self.area}
+                   rooms: {self.rooms}
+                   price: {self.price}
+                   address: {self.address}
+                   plot: {self.plot}
+               """
+
+
+class Flat(Property):
+    def __init__(self, floor: int, area: str, rooms: int, price: decimal, address: str):
+        super().__init__(area, rooms, price, address)
+        self.floor = floor
+
+    def __str__(self):
+        return f"""
+               Flat(Property):
+                    area: {self.area}
+                   rooms: {self.rooms}
+                   price: {self.price}
+                   address: {self.address}
+                   floor: {self.floor}
+               """
+
+
+dom = House(area="xyz", price=3456.20, address="costam", rooms=10, plot=4)
+mieszkanie = Flat(floor=3,area="xyz2",rooms=3,price=123.10,address="costam2")
+print(dom)
+print(mieszkanie)
